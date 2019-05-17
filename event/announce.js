37350c1463996a71b6e9ce5ea6c52065402601d3
Vince Navarro <vincenavarro@gmail.com>
2019-05-16 20:53:12 -0400

    💬 Clean up announce box

--- event/announce.js
+++ event/announce.js
@@ -5,18 +5,16 @@ module.exports = (client, channel) => {
   const { enabled, channelName, category, message, stream = '' } = channel;
   const discordChannel = client.config.announce[category];
 
-  //console.log(channel.stream);
-
   //https://discord.js.org/#/docs/main/stable/class/RichEmbed
   const embed = new RichEmbed()
     // Set the title of the field
-    .setTitle(`${stream.channel.display_name} is streaming ${stream.channel.status}!`)
+    .setTitle(`${stream.channel.display_name} - ${stream.channel.status}`)
     // Set the color of the embed
     .setColor(0xff0000)
     .setURL(stream.channel.url)
     .setThumbnail(stream.channel.logo)
     // Set the main content of the embed
-    .setDescription(`Watch here ${stream.channel.url}! ${message}`)
+    .setDescription(`@here! Click above to watch! ${message}`)
     .attachFiles(['./image/bomb.png'])
     .setFooter(
       `This message will self-destruct ${moment()
