0ab910aa5f9297c7822e7d3b3b7b3a5477192164
Vince Navarro <vincenavarro@gmail.com>
2019-06-05 21:24:12 -0400

    🐛 No longer crashes if internet is down on init

--- index.js
+++ index.js
@@ -10,8 +10,37 @@ client.config = require('./config.json');
 // Add global functions like crash handling, event loading, etc.
 require('./function/core.js')(client);
 
-const startBot = async () => {
-  client.logger.log(`Electric Bovine starting.`);
+const botLogin = () => {
+  client.logger.log(`Electric Bovine attempting login.`);
+  client
+    .login(client.config.discordToken)
+    .then(() => {
+      // Set bot status.
+      // Make the bot "play the game" which is the help command with default prefix.
+      client.user.setActivity(`Milk`);
+      client.user.setStatus('online');
+      client.user.setPresence({
+        game: {
+          name: 'Milk',
+          type: 'STREAMING', // PLAYING, STREAMING, LISTENING, WATCHING
+          url: 'https://twitch.tv/overwatchleague',
+        },
+        status: 'idle',
+      });
+      // Start TwitchCHeck
+      if (client.config.twitch.enabled) {
+        require('./function/twitch.js')(client);
+        client.twitchCheck(client.config.twitch.token, client.config.twitch.channels);
+      }
+    })
+    .catch(error => {
+      client.logger.error(`Error. Attempting again in thirty seconds. ${error}`);
+      setTimeout(botLogin, 30 * 1000);
+    });
+};
+
+const botLoad = async () => {
+  client.logger.log(`Electric Bovine is loading.`);
 
   client.loadAllCommands('./command/').then(results => {
     client.logger.log(`${results.loaded.length} commands loaded: ${results.loaded.join(', ')}`);
@@ -24,26 +53,7 @@ const startBot = async () => {
   });
 
   // Make it so...
-  client.login(client.config.discordToken).then(() => {
-    // Set bot status.
-    // Make the bot "play the game" which is the help command with default prefix.
-    client.user.setActivity(`Milk`);
-    client.user.setStatus('online');
-    client.user.setPresence({
-      game: {
-        name: 'Milk',
-        type: 'STREAMING', // PLAYING, STREAMING, LISTENING, WATCHING
-        url: 'https://twitch.tv/overwatchleague',
-      },
-      status: 'idle',
-    });
-
-    // Start TwitchCHeck
-    if (client.config.twitch.enabled) {
-      require('./function/twitch.js')(client);
-      client.twitchCheck(client.config.twitch.token, client.config.twitch.channels);
-    }
-  });
+  botLogin();
 };
 
 // TODO: Handle what to do when a bot does not have permissions to speak in a channel.
@@ -53,4 +63,4 @@ process.on('unhandledRejection', (reason, p) => {
   // application specific logging, throwing an error, or other logic here
 });
 
-startBot();
+botLoad();
